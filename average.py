365c141ae0c7363db6a7ea0347e227ea61f9a23c
Casey Kuball <darthfett@gmail.com>
2012-09-19 16:15:06 -0700

    Average functionality implemented and working

---
+++ average.py
@@ -0,0 +1,41 @@
+from __future__ import division
+from __future__ import print_function
+
+import colormodel as cm
+
+#########################################
+#
+# Temporarily Copied from nearest.py
+#
+def average_RGB(img):
+    """
+    Get the average RGB of the image, as a tuple.
+    
+    The value returned for each component is a fractional portion of the max red/green/blue.
+    
+    For example,
+    (1, 1, 1) is white,
+    (0, 0, 0) is black.
+    """
+    
+    # Sum all the Reds, Greens, and Blues
+    pixels = ((px.red, px.green, px.blue) for row in img for px in row)
+    total = tuple(sum(colors) for colors in zip(*pixels))
+
+    # Find average RGB
+    num_pixels = img.width * img.height
+    avg = tuple((component / num_pixels) / (2**16 - 1) for component in total)
+
+    return avg
+#
+#
+#########################################
+
+def print_average(images, color_model):
+    if color_model not in cm.converter_for_colormodel:
+        raise ValueError("print_average: Invalid color model {cm}".format(cm=color_model))
+    average_RGBs = [average_RGB(image) for image in images]
+    print("Average {cm} color:".format(cm=color_model))
+    for cell, avg in enumerate(average_RGBs):
+        print("\tCell {cell}: {color}".format(cell=cell+1, color=cm.converter_for_colormodel[color_model](*avg)))
+            
