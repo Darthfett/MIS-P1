48f162ce68f141b1537fba86dc4884ac66767315
Casey Kuball <darthfett@gmail.com>
2012-09-19 16:02:19 -0700

    add converter, remove unicode characters to avoid compile errors

--- colormodel.py
+++ colormodel.py
@@ -1,10 +1,10 @@
 def RGB_to_XYZ(r, g, b):
     """Get a tuple of the X, Y, Z component of the color."""
-    
+
     # Copied from book/notes:
-    # X = 0.607 · R + 0.174 · G + 0.200 · B
-    # Y = 0.299 · R + 0.587 · G + 0.114 · B
-    # Z = 0.000 · R + 0.066 · G + 1.116 · B
+    # X = 0.607 * R + 0.174 * G + 0.200 * B
+    # Y = 0.299 * R + 0.587 * G + 0.114 * B
+    # Z = 0.000 * R + 0.066 * G + 1.116 * B
 
     x = 0.607 * r + 0.174 * g + 0.200 * b
     y = 0.299 * r + 0.587 * g + 0.114 * b
@@ -38,22 +38,22 @@ def RGB_to_LAB(r, g, b, xn=DEFAULT_XYZ, yn=DEFAULT_XYZ, zn=DEFAULT_XYZ):
     x = 0.3935 * r + 0.3653 * g + 0.1916 * b
     y = 0.2124 * r + 0.7011 * g + 0.0866 * b
     z = 0.0187 * r + 0.1119 * g + 0.9582 * b
-    
+
     rel_x = (x / xn) ** (1 / 3)
     rel_y = (y / yn) ** (1 / 3)
     rel_z = (z / zn) ** (1 / 3)
-    
+
     L = 116 * (rel_y) - 16
     a = 500 * (rel_x - rel_y)
     b = 500 * (rel_y - rel_z)
-    
+
     return (L, a, b)
 
 def RGB_to_HSL(r, g, b):
     max_ = max(r, g, b)
     min_ = min(r, g, b)
     chroma = max_ - min_
-    
+
     if chroma == 0:
         H = None
     else:
@@ -64,11 +64,22 @@ def RGB_to_HSL(r, g, b):
         else:
             H = ((r - g) / c) + 4
         H *= 60
-        
+
     L = (.5) * (max_ + min_)
     if chroma == 0:
         S = 0
     else:
         S = chroma / (1 - abs(2 * L - 1))
-    
+
     return (H, S, L)
+
+converter_for_colormodel = {
+    'RGB': lambda x: x,
+    'XYZ': RGB_to_XYZ,
+    'CMY': RGB_to_CMY,
+    'YUV': RGB_to_YUV,
+    'YIQ': RGB_to_YIQ,
+    'YCbCr': RGB_to_YCbCr,
+    'Lab': RGB_to_LAB,
+    'HSL': RGB_to_HSL,
+}
